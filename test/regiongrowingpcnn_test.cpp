c30dcc51b2df364dd7b1963dd7d093c66329cf3e
Rafal Pietruch <rpietruc@interia.pl>
2014-09-13 07:09:38 +0200

    new region growing test added

--- test/regiongrowingpcnn_test.cpp
+++ test/regiongrowingpcnn_test.cpp
@@ -14,12 +14,12 @@ struct IplImageDeleter
         }
     };
 
-CvSize size = cvSize(2, 2);
-
 BOOST_AUTO_TEST_CASE(workingThresholdTest)
     {
+    CvSize size = cvSize(2, 2);
+
     shared_ptr<IplImage> P(cvCreateImage(size, IPL_DEPTH_8U, 1), IplImageDeleter());
-    shared_ptr<IplImage> G(cvCreateImage(size, IPL_DEPTH_8U, 1), IplImageDeleter());
+    shared_ptr<IplImage> G(cvCreateImage(size, IPL_DEPTH_32F, 1), IplImageDeleter());
     cvSet2D(P.get(), 0, 0, cvRealScalar(0));
     cvSet2D(P.get(), 0, 1, cvRealScalar(0));
     cvSet2D(P.get(), 1, 0, cvRealScalar(0));
@@ -33,31 +33,60 @@ BOOST_AUTO_TEST_CASE(workingThresholdTest)
     BOOST_CHECK_EQUAL(workingThreshold(G.get(), P.get()), 3);
     }
 
-/**
-  * Feeding:
-  * \f$ L_x[t] = \sum_{z \in N(x)} Y_z[t] - d \f$.
-  */
 BOOST_AUTO_TEST_CASE(feedingTest)
     {
+    CvSize size = cvSize(3, 3);
+
     shared_ptr<IplImage> Y(cvCreateImage(size, IPL_DEPTH_8U, 1), IplImageDeleter());
-    shared_ptr<IplImage> L(cvCreateImage(size, IPL_DEPTH_8U, 1), IplImageDeleter());
+    shared_ptr<IplImage> L(cvCreateImage(size, IPL_DEPTH_32F, 1), IplImageDeleter());
+
+    // 1 2 3
+    // 4 0 5
+    // 6 7 8
     cvSet2D(Y.get(), 0, 0, cvRealScalar(1));
     cvSet2D(Y.get(), 0, 1, cvRealScalar(2));
-    cvSet2D(Y.get(), 1, 0, cvRealScalar(3));
-    cvSet2D(Y.get(), 1, 1, cvRealScalar(4));
+    cvSet2D(Y.get(), 0, 2, cvRealScalar(3));
+    cvSet2D(Y.get(), 1, 0, cvRealScalar(4));
+    cvSet2D(Y.get(), 1, 1, cvRealScalar(0));
+    cvSet2D(Y.get(), 1, 2, cvRealScalar(5));
+    cvSet2D(Y.get(), 2, 0, cvRealScalar(6));
+    cvSet2D(Y.get(), 2, 1, cvRealScalar(7));
+    cvSet2D(Y.get(), 2, 2, cvRealScalar(8));
 
     feeding(Y.get(), L.get(), 1);
 
-    BOOST_CHECK_EQUAL(cvGet2D(L.get(), 0, 0).val[0], 8);
-    BOOST_CHECK_EQUAL(cvGet2D(L.get(), 0, 1).val[0], 7);
-    BOOST_CHECK_EQUAL(cvGet2D(L.get(), 1, 0).val[0], 6);
-    BOOST_CHECK_EQUAL(cvGet2D(L.get(), 1, 1).val[0], 5);
+    BOOST_CHECK_EQUAL(cvGet2D(L.get(), 1, 1).val[0], 35);
+    }
+
+BOOST_AUTO_TEST_CASE(linkingTest)
+    {
+    CvSize size = cvSize(2, 2);
+    double beta_t = 0.5;
+
+    shared_ptr<IplImage> L(cvCreateImage(size, IPL_DEPTH_32F, 1), IplImageDeleter());
+    shared_ptr<IplImage> G(cvCreateImage(size, IPL_DEPTH_32F, 1), IplImageDeleter());
+    shared_ptr<IplImage> U(cvCreateImage(size, IPL_DEPTH_32F, 1), IplImageDeleter());
+
+    cvSet2D(L.get(), 0, 0, cvRealScalar(0));
+    cvSet2D(L.get(), 0, 1, cvRealScalar(1));
+    cvSet2D(L.get(), 1, 0, cvRealScalar(2));
+    cvSet2D(L.get(), 1, 1, cvRealScalar(3));
+    cvSet(G.get(), cvRealScalar(2));
+
+    linking(L.get(), G.get(), U.get(), beta_t);
+
+    BOOST_CHECK_EQUAL(cvGet2D(U.get(), 0, 0).val[0], 2);
+    BOOST_CHECK_EQUAL(cvGet2D(U.get(), 0, 1).val[0], 3);
+    BOOST_CHECK_EQUAL(cvGet2D(U.get(), 1, 0).val[0], 4);
+    BOOST_CHECK_EQUAL(cvGet2D(U.get(), 1, 1).val[0], 5);
     }
 
 BOOST_AUTO_TEST_CASE(pulseOutputTest)
     {
-    shared_ptr<IplImage> U(cvCreateImage(size, IPL_DEPTH_8U, 1), IplImageDeleter());
-    shared_ptr<IplImage> T(cvCreateImage(size, IPL_DEPTH_8U, 1), IplImageDeleter());
+    CvSize size = cvSize(2, 2);
+
+    shared_ptr<IplImage> U(cvCreateImage(size, IPL_DEPTH_32F, 1), IplImageDeleter());
+    shared_ptr<IplImage> T(cvCreateImage(size, IPL_DEPTH_32F, 1), IplImageDeleter());
     shared_ptr<IplImage> Y(cvCreateImage(size, IPL_DEPTH_8U, 1), IplImageDeleter());
 
     cvSet2D(U.get(), 0, 0, cvRealScalar(0));
